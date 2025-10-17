0a56d3ba6299067b8a91feebdb33907212087fb8
yang2333qwq <125105117+yang2333qwq@users.noreply.github.com>
2025-10-17 23:15:14 +0800

    Create package.js

---
+++ package.js
@@ -0,0 +1,20 @@
+{
+  "name": "goal-tracker-uni",
+  "version": "1.0.0",
+  "description": "基于uni-app+Vue3的目标督促小程序",
+  "main": "main.js",
+  "scripts": {
+    "dev:mp-weixin": "uni dev --platform mp-weixin",
+    "build:mp-weixin": "uni build --platform mp-weixin"
+  },
+  "dependencies": {
+    "vue": "^3.3.4",
+    "@dcloudio/uni-app": "^2.0.0"
+  },
+  "devDependencies": {
+    "@dcloudio/types": "^3.3.2",
+    "@vue/runtime-core": "^3.3.4",
+    "typescript": "^5.1.6",
+    "vue-tsc": "^1.8.5"
+  }
+}
