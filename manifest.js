01cd0f8bdee70dbfc6d09ca7b20fcd49e730fdc9
yang2333qwq <125105117+yang2333qwq@users.noreply.github.com>
2025-10-17 23:16:16 +0800

    Add initial manifest.js for uni-app configuration

---
+++ manifest.js
@@ -0,0 +1,21 @@
+{
+  "name": "目标督促助手",
+  "appid": "__UNI__XXXXXX", // 替换为你的uni-app AppID（HBuilderX中获取）
+  "description": "",
+  "versionName": "1.0.0",
+  "versionCode": "100",
+  "minSdkVersion": "2.0.0",
+  "uniStatistics": {
+    "enable": false
+  },
+  "mp-weixin": {
+    "appid": "wxXXXXXX", // 替换为你的微信小程序AppID
+    "setting": {
+      "urlCheck": false,
+      "es6": true,
+      "postcss": true
+    },
+    "usingComponents": true
+  },
+  "vueVersion": "3"
+}
